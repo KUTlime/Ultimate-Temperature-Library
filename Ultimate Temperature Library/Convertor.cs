1fa333ac695f96d0e6ebb0cd236a1840fa35d228
Radek Zahradník <kutlime@gmail.com>
2019-05-26 23:52:50 +0200

    The converter class methods help update

--- Ultimate Temperature Library/Convertor.cs
+++ Ultimate Temperature Library/Convertor.cs
@@ -5,281 +5,562 @@
     /// </summary>
     public static class Convertor
     {
+        /// <summary>
+        /// Converts a double value in Celsius to double value in Kelvin.
+        /// </summary>
+        /// <param name="value">A value in Celsius.</param>
+        /// <returns>A value in Kelvin.</returns>
         public static double Cel2Kel(double value)
         {
             return value + Constants.AbsoluteZeroInCelsius;
         }
 
+        /// <summary>
+        /// Converts a double value in Celsius to double value in Fahrenheit.
+        /// </summary>
+        /// <param name="value">A value in Celsius.</param>
+        /// <returns>A value in Fahrenheit.</returns>
         public static double Cel2Fah(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Celsius to double value in Rankine.
+        /// </summary>
+        /// <param name="value">A value in Celsius.</param>
+        /// <returns>A value in Rankine.</returns>
         public static double Cel2Ran(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Celsius to double value in Delisle.
+        /// </summary>
+        /// <param name="value">A value in Celsius.</param>
+        /// <returns>A value in Delisle.</returns>
         public static double Cel2Del(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Celsius to double value in Newton.
+        /// </summary>
+        /// <param name="value">A value in Celsius.</param>
+        /// <returns>A value in Newton.</returns>
         public static double Cel2New(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Celsius to double value in Réaumur.
+        /// </summary>
+        /// <param name="value">A value in Celsius.</param>
+        /// <returns>A value in Réaumur.</returns>
         public static double Cel2Réau(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Celsius to double value in Rømer.
+        /// </summary>
+        /// <param name="value">A value in Celsius.</param>
+        /// <returns>A value in Rømer.</returns>
         public static double Cel2Røm(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Fahrenheit to double value in Kelvin.
+        /// </summary>
+        /// <param name="value">A value in Fahrenheit.</param>
+        /// <returns>A value in Kelvin.</returns>
         public static double Fah2Kel(double value)
         {
             return value + Constants.AbsoluteZeroInCelsius;
         }
 
+        /// <summary>
+        /// Converts a double value in Fahrenheit to double value in Celsius.
+        /// </summary>
+        /// <param name="value">A value in Fahrenheit.</param>
+        /// <returns>A value in Celsius.</returns>
         public static double Fah2Cel(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Fahrenheit to double value in Rankine.
+        /// </summary>
+        /// <param name="value">A value in Fahrenheit.</param>
+        /// <returns>A value in Rankine.</returns>
         public static double Fah2Ran(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Fahrenheit to double value in Delisle.
+        /// </summary>
+        /// <param name="value">A value in Fahrenheit.</param>
+        /// <returns>A value in Delisle.</returns>
         public static double Fah2Del(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Fahrenheit to double value in Newton.
+        /// </summary>
+        /// <param name="value">A value in Fahrenheit.</param>
+        /// <returns>A value in Newton.</returns>
         public static double Fah2New(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Fahrenheit to double value in Réaumur.
+        /// </summary>
+        /// <param name="value">A value in Fahrenheit.</param>
+        /// <returns>A value in Réaumur.</returns>
         public static double Fah2Réau(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Fahrenheit to double value in Rømer.
+        /// </summary>
+        /// <param name="value">A value in Fahrenheit.</param>
+        /// <returns>A value in Rømer.</returns>
         public static double Fah2Røm(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Kelvin to double value in Celsius.
+        /// </summary>
+        /// <param name="value">A value in Kelvin.</param>
+        /// <returns>A value in Celsius.</returns>
         public static double Kel2Cel(double value)
         {
             return value - Constants.AbsoluteZeroInCelsius;
         }
 
+        /// <summary>
+        /// Converts a double value in Kelvin to double value in Fahrenheit.
+        /// </summary>
+        /// <param name="value">A value in Kelvin.</param>
+        /// <returns>A value in Fahrenheit.</returns>
         public static double Kel2Fah(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Kelvin to double value in Rankine.
+        /// </summary>
+        /// <param name="value">A value in Kelvin.</param>
+        /// <returns>A value in Rankine.</returns>
         public static double Kel2Ran(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Kelvin to double value in Delisle.
+        /// </summary>
+        /// <param name="value">A value in Kelvin.</param>
+        /// <returns>A value in Delisle.</returns>
         public static double Kel2Del(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Kelvin to double value in Newton.
+        /// </summary>
+        /// <param name="value">A value in Kelvin.</param>
+        /// <returns>A value in Newton.</returns>
         public static double Kel2New(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Kelvin to double value in Réaumur.
+        /// </summary>
+        /// <param name="value">A value in Kelvin.</param>
+        /// <returns>A value in Réaumur.</returns>
         public static double Kel2Réau(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Kelvin to double value in Rømer.
+        /// </summary>
+        /// <param name="value">A value in Kelvin.</param>
+        /// <returns>A value in Rømer.</returns>
         public static double Kel2Røm(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Rankine to double value in Kelvine.
+        /// </summary>
+        /// <param name="value">A value in Rankine.</param>
+        /// <returns>A value in Celsius.</returns>
         public static double Ran2Kel(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Rankine to double value in Celsius.
+        /// </summary>
+        /// <param name="value">A value in Rankine.</param>
+        /// <returns>A value in Celsius.</returns>
         public static double Ran2Cel(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Rankine to double value in Fahrenheit.
+        /// </summary>
+        /// <param name="value">A value in Rankine.</param>
+        /// <returns>A value in Fahrenheit.</returns>
         public static double Ran2Fah(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Rankine to double value in Delisle.
+        /// </summary>
+        /// <param name="value">A value in Rankine.</param>
+        /// <returns>A value in Delisle.</returns>
         public static double Ran2Del(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Rankine to double value in Newton.
+        /// </summary>
+        /// <param name="value">A value in Rankine.</param>
+        /// <returns>A value in Newton.</returns>
         public static double Ran2New(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Rankine to double value in Réaumur.
+        /// </summary>
+        /// <param name="value">A value in Rankine.</param>
+        /// <returns>A value in Réaumur.</returns>
         public static double Ran2Réau(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Rankine to double value in Rømer.
+        /// </summary>
+        /// <param name="value">A value in Rankine.</param>
+        /// <returns>A value in Rømer.</returns>
         public static double Ran2Røm(double value)
         {
             return 0;
         }
 
+
+        /// <summary>
+        /// Converts a double value in Delisle to double value in Kelvin.
+        /// </summary>
+        /// <param name="value">A value in Delisle.</param>
+        /// <returns>A value in Kelvin.</returns>
         public static double Del2Kel(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Delisle to double value in Celsius.
+        /// </summary>
+        /// <param name="value">A value in Delisle.</param>
+        /// <returns>A value in Celsius.</returns>
         public static double Del2Cel(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Delisle to double value in Fahrenheit.
+        /// </summary>
+        /// <param name="value">A value in Delisle.</param>
+        /// <returns>A value in Fahrenheit.</returns>
         public static double Del2Fah(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Delisle to double value in Rankine.
+        /// </summary>
+        /// <param name="value">A value in Delisle.</param>
+        /// <returns>A value in Rankine.</returns>
         public static double Del2Ran(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Delisle to double value in Newton.
+        /// </summary>
+        /// <param name="value">A value in Delisle.</param>
+        /// <returns>A value in Newton.</returns>
         public static double Del2New(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Delisle to double value in Réaumur.
+        /// </summary>
+        /// <param name="value">A value in Delisle.</param>
+        /// <returns>A value in Réaumur.</returns>
         public static double Del2Réau(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Delisle to double value in Rømer.
+        /// </summary>
+        /// <param name="value">A value in Delisle.</param>
+        /// <returns>A value in Rømer.</returns>
         public static double Del2Røm(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Newton to double value in Kelvin.
+        /// </summary>
+        /// <param name="value">A value in Newton.</param>
+        /// <returns>A value in Kelvin.</returns>
         public static double New2Kel(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Newton to double value in Celsius.
+        /// </summary>
+        /// <param name="value">A value in Newton.</param>
+        /// <returns>A value in Celsius.</returns>
         public static double New2Cel(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Newton to double value in Fahrenheit.
+        /// </summary>
+        /// <param name="value">A value in Newton.</param>
+        /// <returns>A value in Fahrenheit.</returns>
         public static double New2Fah(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Newton to double value in Rankine.
+        /// </summary>
+        /// <param name="value">A value in Newton.</param>
+        /// <returns>A value in Rankine.</returns>
         public static double New2Ran(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Newton to double value in Delisle.
+        /// </summary>
+        /// <param name="value">A value in Newton.</param>
+        /// <returns>A value in Delisle.</returns>
         public static double New2Del(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Newton to double value in Réaumur.
+        /// </summary>
+        /// <param name="value">A value in Newton.</param>
+        /// <returns>A value in Réaumur.</returns>
         public static double New2Réau(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Newton to double value in Rømer.
+        /// </summary>
+        /// <param name="value">A value in Newton.</param>
+        /// <returns>A value in Rømer.</returns>
         public static double New2Røm(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Réaumur to double value in Kelvin.
+        /// </summary>
+        /// <param name="value">A value in Réaumur.</param>
+        /// <returns>A value in Kelvin.</returns>
         public static double Réau2Kel(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Réaumur to double value in Celsius.
+        /// </summary>
+        /// <param name="value">A value in Réaumur.</param>
+        /// <returns>A value in Celsius.</returns>
         public static double Réau2Cel(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Réaumur to double value in Fahrenheit.
+        /// </summary>
+        /// <param name="value">A value in Réaumur.</param>
+        /// <returns>A value in Fahrenheit.</returns>
         public static double Réau2Fah(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Réaumur to double value in Rankine.
+        /// </summary>
+        /// <param name="value">A value in Réaumur.</param>
+        /// <returns>A value in Rankine.</returns>
         public static double Réau2Ran(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Réaumur to double value in Delisle.
+        /// </summary>
+        /// <param name="value">A value in Réaumur.</param>
+        /// <returns>A value in Delisle.</returns>
         public static double Réau2Del(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Réaumur to double value in Newton.
+        /// </summary>
+        /// <param name="value">A value in Réaumur.</param>
+        /// <returns>A value in Newton.</returns>
         public static double Réau2New(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Réaumur to double value in Rømer.
+        /// </summary>
+        /// <param name="value">A value in Réaumur.</param>
+        /// <returns>A value in Rømer.</returns>
         public static double Réau2Røm(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Rømer to double value in Kelvin.
+        /// </summary>
+        /// <param name="value">A value inRømer.</param>
+        /// <returns>A value in Kelvin.</returns>
         public static double Røm2Kel(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Rømer to double value in Celsius.
+        /// </summary>
+        /// <param name="value">A value inRømer.</param>
+        /// <returns>A value in Celsius.</returns>
         public static double Røm2Cel(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Rømer to double value in Fahrenheit.
+        /// </summary>
+        /// <param name="value">A value inRømer.</param>
+        /// <returns>A value in Fahrenheit.</returns>
         public static double Røm2Fah(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Rømer to double value in Rankine.
+        /// </summary>
+        /// <param name="value">A value inRømer.</param>
+        /// <returns>A value in Rankine.</returns>
         public static double Røm2Ran(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Rømer to double value in Delisle.
+        /// </summary>
+        /// <param name="value">A value inRømer.</param>
+        /// <returns>A value in Delisle.</returns>
         public static double Røm2Del(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Rømer to double value in Newton.
+        /// </summary>
+        /// <param name="value">A value inRømer.</param>
+        /// <returns>A value in Newton.</returns>
         public static double Røm2New(double value)
         {
             return 0;
         }
 
+        /// <summary>
+        /// Converts a double value in Rømer to double value in Réaumur.
+        /// </summary>
+        /// <param name="value">A value inRømer.</param>
+        /// <returns>A value in Réaumur.</returns>
         public static double Røm2Réau(double value)
         {
             return 0;
